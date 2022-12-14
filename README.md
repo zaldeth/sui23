9a865bf1a7b96cb9aed3a649bf8fc193f87a30cd
zaldeth <113926156+zaldeth@users.noreply.github.com>
2022-12-14 21:16:05 +0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
-# Project 23
 
-Project Solution 23
--- sketch.js
+++ sketch.js
@@ -8,6 +8,8 @@ var canvas;
 var palyer, playerBase;
 var computer, computerBase;
 
+var arrow;
+
 
 function setup() {
   canvas = createCanvas(windowWidth, windowHeight);
@@ -15,9 +17,15 @@ function setup() {
   engine = Engine.create();
   world = engine.world;
 
-  playerBase = new PlayerBase(290, random(450, height - 300), 180, 150);
-//create a player object from the Player class.
- player = new Player(285, playerBase.body.position.y - 153, 50, 180);
+  playerBase = new PlayerBase(300, random(450, height - 300), 180, 150);
+  player = new Player(285, playerBase.body.position.y - 153, 50, 180);
+  playerArcher = new PlayerArcher(
+    340,
+    playerBase.body.position.y - 180,
+    120,
+    120
+  );
+
   computerBase = new ComputerBase(
     width - 300,
     random(450, height - 300),
@@ -30,10 +38,19 @@ function setup() {
     50,
     180
   );
+  computerArcher = new ComputerArcher(
+    width - 340,
+    computerBase.body.position.y - 180,
+    120,
+    120
+  );
+  
+  arrow = new PlayerArrow(playerArcher.body.position.x, playerArcher.body.position.y, 100, 10);
+  
 }
 
 function draw() {
-  background(189);
+  background(180);
 
   Engine.update(engine);
 
@@ -45,11 +62,29 @@ function draw() {
 
  
   playerBase.display();
-//call the display() function for the player object.
   player.display();
+  
 
   computerBase.display();
   computer.display();
   
+  playerArcher.display();
+  computerArcher.display()
+  
+  // if(keyCode === 32){
+  //   arrow.display()
+  //   arrow.shoot(playerArcher);
+  //  }
 
+  //  if(keyCode === 32){
+  //   arrow.shoot(playerArcher.body.angle);
+  //  }
+   
+   if(keyCode === 32){
+     arrow.display()
+     arrow.shoot(playerArcher.body.angle);
+    }
 }
+
+
+
